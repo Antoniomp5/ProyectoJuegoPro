625b3f77ab59621dc6c56b0dc8d2d55e3dc27aa7
Antoniomp5 <antoniohg2001@gmail.com>
2025-05-08 12:59:09 +0200

    Add files via upload

---
+++ juego.java
@@ -0,0 +1,22 @@
+package juego;
+
+import java.util.Scanner;
+
+public class juego {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("¡Bienvenido al...S!");
+        System.out.println("Introduce tu nombre: ");
+        
+		Scanner sc = new Scanner(System.in);
+		
+		String nombre = sc.nextLine();
+		
+		
+		System.out.println("Hola: " + nombre );
+		
+
+	}
+
+}
